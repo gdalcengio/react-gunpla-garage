d8e4abd18482677b95c0810f2c54b42e32e1ed99
dalcengio.gabriele@gmail.com <gdalceng@sfu.ca>
2021-12-10 15:42:14 -0800

    Add material icons for the shopping cart icon and a signup button

--- gunpla-store/src/components/Header.js
+++ gunpla-store/src/components/Header.js
@@ -1,9 +1,18 @@
 import "./Header.css";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 function Header(props) {
   return (
     <header className="header">
       <h1 className="header-title">Gabe's Gunpla Garage</h1>
+      <div className="user-buttons">
+        <a href="#/cart">
+          <ShoppingCartIcon sx={{ color: "black" }}></ShoppingCartIcon>
+        </a>
+        <a href="#/signup" className="sign-up-button">
+          Sign Up
+        </a>
+      </div>
     </header>
   );
 }
