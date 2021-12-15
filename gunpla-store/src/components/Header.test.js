014963b3e7a1801c0713a5bb0bf10bd6ced42936
dalcengio.gabriele@gmail.com <gdalceng@sfu.ca>
2021-12-15 11:28:12 -0800

    Test shopping cart rendering

--- gunpla-store/src/components/Header.test.js
+++ gunpla-store/src/components/Header.test.js
@@ -1,6 +1,14 @@
 import Header from "./Header";
 import { mount, shallow } from "enzyme";
+import { screen } from "@testing-library/dom";
 
 it("renders without crashing", () => {
   shallow(<Header />);
+});
+
+it("renders the svg without crashing", () => {
+  const wrapper = mount(<Header />);
+  const element = screen.queryByTestId("#ShoppingCartIcon");
+
+  expect(wrapper.contains(element));
 });
